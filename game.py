0204b0edd57cae4db30d5cc58ba983ece63dd2bb
Tony Chang <chc012@ucsd.edu>
2020-09-22 23:59:04 -0700

    construct board, main contructor

--- game.py
+++ game.py
@@ -3,45 +3,31 @@ Main entry for the Chinese Chess game. For game rule, please see README.
 """
 __author__ = "Tony Chang"
 
-import pygame
 import os
+from tkinter import *
 from abc import ABC, abstractmethod
 
 ASSET_PATH = "assets"
-BOARD = "board.jpg"
+BOARD_IMG = "board.jpg"
 
 SPEED = 10
-SCR_SIZE = (SCR_WIDTH, SCR_HEIGHT) = (1100, 1000)
-BOARD_SIZE = (BOARD_WIDTH, BOARD_HEIGHT) = ((SCR_WIDTH/11)*9, SCR_HEIGHT)
-PIECE_SIZE = (PIECE_WIDTH, PIECE_HEIGHT) = (BOARD_WIDTH/9, BOARD_HEIGHT/10)
+SCR_SIZE = (SCR_WIDTH, SCR_HEIGHT) = (900 + 300, 1000)
+BOARD_SIZE = (BOARD_WIDTH, BOARD_HEIGHT) = (int(SCR_WIDTH / 4 * 3), SCR_HEIGHT)
+PIECE_SIZE = (PIECE_WIDTH, PIECE_HEIGHT) = \
+        (min(int(BOARD_WIDTH / 9 * 0.98), int(BOARD_HEIGHT / 10 * 0.98)), ) * 2
+BORDER_WIDTH = 20
 
-pygame.mixer.pre_init(44100, -16, 2, 2048)
-pygame.init()
-pygame.display.set_caption("Chinese Chess")
+WHITE = "#fff"
 
-SCREEN = pygame.display.set_mode(SCR_SIZE)
-
-# Sound effect
-# TODO: find sounds
-MOVE_SOUND = pygame.mixer.Sound(os.path.join(ASSET_PATH, "move.wav"))
-TAKE_SOUND = pygame.mixer.Sound(os.path.join(ASSET_PATH, "take.wav"))
-WIN_SOUND = pygame.mixer.Sound(os.path.join(ASSET_PATH, "win.wav"))
-
-def load_image(path, size=(-1, -1)):
-    """Set up picture and size conversion"""
-    image = pygame.image.load(os.path.join(ASSET_PATH, path))
-    image = image.convert()
-    if size != (-1, -1):
-        image = pygame.transform.scale(image, size)
-    return (image, image.get_rect())
-
-
+# TODO
 class Board():
     def __init__(self):
-        self.image, self.rect = load_image(BOARD, BOARD_SIZE)
+
 
     pass
 
+
+# TODO
 class Piece(ABC):
     """Abstract chess pieces, set up initialization and taking."""
     def __init__(self, piece, position):
@@ -50,32 +36,95 @@ class Piece(ABC):
 
     @abstractmethod
     def move(self):
+        """How the specific piece will move according to Chinese Chess rules."""
+        pass
+
+    def die(self, opponent):
+        """Die from opponent's attack."""
+
         pass
 
 
-    pass
-
+# TODO
 class Pawn():
     pass
 
+
+# TODO
 class King():
     pass
 
+
+# TODO
 class Guard():
     pass
 
 
+# TODO
 class Minister():
     pass
 
 
+# TODO
 class Cannon():
     pass
 
 
+# TODO
 class Knight():
     pass
 
 
+# TODO
 class Chariot():
     pass
+
+
+# TODO
+class ChineseChess():
+    """Main class. Set up game and run it."""
+    # TODO: make a menu system
+    def __init__(self):
+        """Make window, board, pieces, map."""
+        self.window = Tk()
+        self.window.title("Chinese Chess")
+        self.window.iconbitmap("icon.ico")
+        self.window.geometry(str(SCR_WIDTH) + 'x' + str(SCR_HEIGHT))
+
+        self.board = Canvas(window, width=BOARD_WIDTH, height=BOARD_HEIGHT, bg=WHITE,
+                            bd=BORDER_WIDTH)
+        board.pack()
+        bg = PhotoImage(file=BOARD_IMG)
+        self.board_img = self.board.create_image(BOARD_WIDTH, BOARD_HEIGHT, image=bg)
+
+        self.pieces = {
+            pawns: _make_pieces(Pawn, 10),
+            kings: _make_pieces(King, 2),
+            guards: _make_pieces(Guard),
+            ministers: _make_pieces(Minister),
+            cannons: _make_pieces(Cannon),
+            knights: _make_pieces(Knight),
+            chariots: _make_pieces(Chariot)
+        }
+
+        self.map = [[" "]*9]*10
+        for piece_list in self.pieces:
+            for piece in piece_list:
+                self.map[piece.x][piece.y] = piece.icon
+
+
+    def _make_pieces(self, ctor, piece_num=4):
+        """Helper method for pieces dictionary, construct set of pieces."""
+        icon_list = [None] * piece_num
+        for i in range(0, piece_num/2):
+            icon_list[i] = ctor('b', i+1)
+            icon_list[i+piece_num/2] = ctor('r', i+1)
+        return icon_list
+
+
+    def play(self):
+
+
+
+
+        top.mainloop()
