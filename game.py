ae4379b57e0f229042252c6fe7d8962ce8719355
Tony Chang <chc012@ucsd.edu>
2020-09-23 20:39:43 -0700

    Pawn hint

--- game.py
+++ game.py
@@ -3,55 +3,116 @@ Main entry for the Chinese Chess game. For game rule, please see README.
 """
 __author__ = "Tony Chang"
 
-import os
-from tkinter import *
+import tkinter as tk
 from abc import ABC, abstractmethod
+from playsound import playsound
 
-ASSET_PATH = "assets"
-BOARD_IMG = "board.jpg"
+ICON_IMG = "icon.ico"
+BOARD_IMG = "assets/board.jpg"
+PAWN_IMG_B = "assets/pawn_b.jpg"
+PAWN_IMG_R = "assets/pawn_r.jpg"
+KING_IMG_B = "assets/king_b.jpg"
+KING_IMG_R = "assets/king_r.jpg"
+HINT_IMG = "assets/hint.jpg"
 
-SPEED = 10
+MOVE_SOUND = "assets/move.wav"
+TAKE_SOUND = "assets/take.wav"
+WIN_SOUND = "assets/win.wav"
+
 SCR_SIZE = (SCR_WIDTH, SCR_HEIGHT) = (900 + 300, 1000)
 BOARD_SIZE = (BOARD_WIDTH, BOARD_HEIGHT) = (int(SCR_WIDTH / 4 * 3), SCR_HEIGHT)
 PIECE_SIZE = (PIECE_WIDTH, PIECE_HEIGHT) = \
         (min(int(BOARD_WIDTH / 9 * 0.98), int(BOARD_HEIGHT / 10 * 0.98)), ) * 2
 BORDER_WIDTH = 20
+ROW_LENGTH = 9
+COL_LENGTH = 10
+
+TITLE = "Chinese Chess"
+BLACK_SIDE = 'b'
+RED_SIDE = 'r'
+HINT = 'H'
 
 WHITE = "#fff"
 
 # TODO
-class Board():
-    def __init__(self):
-
-
-    pass
-
-
-# TODO
 class Piece(ABC):
     """Abstract chess pieces, set up initialization and taking."""
-    def __init__(self, piece, position):
-        self.image, self.rect = load_image(piece, PIECE_SIZE)
-        self.position = position
+    def __init__(self, board, map, faction, num):
+        """General piece construction. The indicator is made up of a letter and a number, with the
+        letter indicating faction and number indicating number within faction."""
+        self.board = board
+        self.map = map
+        self.faction = faction
+        self.num = num
+        self.pos = (0, 0)
+        if faction == RED_SIDE:
+            self.pos[1] = 9
+
+
+    @abstractmethod
+    def hint(self):
+        """Show the possible movement of specific piece."""
+        pass
+
 
     @abstractmethod
     def move(self):
         """How the specific piece will move according to Chinese Chess rules."""
         pass
 
-    def die(self, opponent):
+
+
+
+
+    def die(self):
         """Die from opponent's attack."""
 
         pass
 
 
 # TODO
-class Pawn():
-    pass
+class Pawn(Piece):
+
+    def __init__(self, board, map, faction, num):
+        """Pawns are represented by 'P' on the map."""
+        super().__init__(board, map, faction, num)
+        self.icon = 'P' + self.faction + str(self.num)
+        if self.faction == BLACK_SIDE:
+            self.pos = ((num - 1) * 2, 3)
+        else:
+            self.pos = ((num - 1) * 2, 6)
+        self.real_x = self.pos[0] + 1
+
+
+
+    def hint(self):
+        """Pawn can only go forward before reaching "the river"; it can go forward, left, and right
+        beyond "the river"."""
+        if self.faction == BLACK_SIDE:
+            if self.pos[1] >= 5:
+                if (self.pos[0] != 0) and (BLACK_SIDE not in self.map[self.pos[0]-1][self.pos[1]]):
+                    self.map[self.pos[0] - 1][self.pos[1]] += HINT
+                if (self.pos[0] != 8) and (BLACK_SIDE not in self.map[self.pos[0]+1][self.pos[1]]):
+                    self.map[self.pos[0] + 1][self.pos[1]] += HINT
+            if (self.pos[1] != 9) and (BLACK_SIDE not in self.map[self.pos[0]][self.pos[1] + 1]):
+                self.map[self.pos[0]][self.pos[1] + 1] += HINT
+        if self.faction == RED_SIDE:
+            if self.pos[1] <= 4:
+                if (self.pos[0] != 0) and (RED_SIDE not in self.map[self.pos[0]-1][self.pos[1]]):
+                    self.map[self.pos[0] - 1][self.pos[1]] += HINT
+                if (self.pos[0] != 8) and (RED_SIDE not in self.map[self.pos[0]+1][self.pos[1]]):
+                    self.map[self.pos[0] + 1][self.pos[1]] += HINT
+            if (self.pos[1] != 0) and (RED_SIDE not in self.map[self.pos[0]][self.pos[1] + 1]):
+                self.map[self.pos[0]][self.pos[1] + 1] += HINT
+
+
+    def move(self):
+        pass
+
 
 
 # TODO
-class King():
+class King(Piece):
     pass
 
 
@@ -85,17 +146,19 @@ class ChineseChess():
     """Main class. Set up game and run it."""
     # TODO: make a menu system
     def __init__(self):
-        """Make window, board, pieces, map."""
-        self.window = Tk()
-        self.window.title("Chinese Chess")
-        self.window.iconbitmap("icon.ico")
+        """Make window, board, map, and pieces; put pieces on map."""
+        self.window = tk.Tk()
+        self.window.title(TITLE)
+        self.window.iconbitmap(ICON_IMG)
         self.window.geometry(str(SCR_WIDTH) + 'x' + str(SCR_HEIGHT))
 
-        self.board = Canvas(window, width=BOARD_WIDTH, height=BOARD_HEIGHT, bg=WHITE,
+        self.board = tk.Canvas(window, width=BOARD_WIDTH, height=BOARD_HEIGHT, bg=WHITE,
                             bd=BORDER_WIDTH)
         board.pack()
-        bg = PhotoImage(file=BOARD_IMG)
+        bg = tk.PhotoImage(file=BOARD_IMG)
         self.board_img = self.board.create_image(BOARD_WIDTH, BOARD_HEIGHT, image=bg)
+
+        self.map = [[" "] * COL_LENGTH] * ROW_LENGTH
 
         self.pieces = {
             pawns: _make_pieces(Pawn, 10),
@@ -107,18 +170,17 @@ class ChineseChess():
             chariots: _make_pieces(Chariot)
         }
 
-        self.map = [[" "]*9]*10
-        for piece_list in self.pieces:
-            for piece in piece_list:
-                self.map[piece.x][piece.y] = piece.icon
+        for piece_list in self.pieces.keys():
+            for piece in self.pieces[piece_list]:
+                self.map[piece.pos[0]][piece.pos[1]] = piece.icon
 
 
     def _make_pieces(self, ctor, piece_num=4):
         """Helper method for pieces dictionary, construct set of pieces."""
         icon_list = [None] * piece_num
         for i in range(0, piece_num/2):
-            icon_list[i] = ctor('b', i+1)
-            icon_list[i+piece_num/2] = ctor('r', i+1)
+            icon_list[i] = ctor(self.board, self.map, BLACK_SIDE, i+1)
+            icon_list[i+piece_num/2] = ctor(self.board, self.map, RED_SIDE, i+1)
         return icon_list
 
 
@@ -127,4 +189,4 @@ class ChineseChess():
 
 
 
-        top.mainloop()
+        self.window.mainloop()
